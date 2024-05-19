41cc4963f80f9ebcb0e03d3f301b8658dc7b725d
parijan57 <khoiri5464@gmail.com>
2024-05-19 15:53:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // index.js
 console.log("Hello, npm project!");
+// index.js
+const _ = require('lodash');
+
+const numbers = [1, 2, 3, 4, 5];
+const sum = _.sum(numbers);
+
+console.log("Sum of numbers:", sum);
